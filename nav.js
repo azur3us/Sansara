54c1648150c61cfca4515f7718bb0ddeb180a9e9
Patryk Binicki <pbinicki@gmail.com>
2019-02-15 20:44:34 +0100

    Commit

--- nav.js
+++ nav.js
@@ -3,6 +3,9 @@ var openBtn = document.getElementById("navbar_icon");
 openBtn.addEventListener("click", openNav, false); 
 var closeBtn = document.getElementById("closebtn"); 
 closeBtn.addEventListener("click", closeNav, false); 
+var menuBtn = document.querySelectorAll('.menu');
+menuBtn.forEach(x => x.addEventListener("click", closeNav, false));
+
 
 
 function openNav() {
@@ -15,7 +18,12 @@ function closeNav() {
   jQuery(function ($) {
     $.scrollTo(0);
 
-    $('link1').click(function () { $.scrollTo($('#myContact'), 500); });
+    $('#link1').click(function () { $.scrollTo($('#Home'), 500); });
+    $('#link2').click(function () { $.scrollTo($('#aboutUs'), 500); });
+    $('#link3').click(function () { $.scrollTo($('#myOffer'), 500); });
+    $('#link4').click(function () { $.scrollTo($('#myRealizations'), 500); });
+    $('#link5').click(function () { $.scrollTo($('#someInfo'), 500); });
+    $('#link6').click(function () { $.scrollTo($('#myContact'), 500); });
 }
 )
  
