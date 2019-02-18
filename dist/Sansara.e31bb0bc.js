5b4edd4b6983b57814d15584490bd0e10f51812a
Patryk Binicki <pbinicki@gmail.com>
2019-02-18 22:14:31 +0100

    Edit start coop btn

--- dist/Sansara.e31bb0bc.js
+++ dist/Sansara.e31bb0bc.js
@@ -11083,7 +11083,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62512" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49259" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
